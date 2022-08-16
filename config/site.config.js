51b5c53f729d5c32a2148704444bb5031f42b1f6
柠萌 <66536447+lemonfunny@users.noreply.github.com>
2022-08-16 16:58:02 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://www.lemonbuluo.top" target="_blank" rel="noopener noreferrer">lemonbuluo</a>',
+    'Powered by <a href="https://www.lemonbuluo.top" target="_blank" rel="noopener noreferrer">lemonbuluo❤2019~2022</a>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -54,10 +54,6 @@ module.exports = {
     {
       name: 'blogs',
       link: 'https://www.lemonbuluo.top',
-    },
-    {
-      name: 'group chat',
-      link: 'https://jq.qq.com/?_wv=1027&k=hdoGVHZt',
     },
   ],
 
